c4548342200089f6003aaa54c357df9cf7f97c88
b1u3too <almadsen792@live.ca>
2022-07-19 03:54:20 +0000

    fix: address nameErrors in email uniqueness example

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -54,10 +54,11 @@ RSpec.describe User, type: :model do
 
     it "should error if email is not unique" do
       first_user = User.new(first_name: "Keanu", last_name: "Reeves", email: "keanuizcool@icloud.me", password: "NEEEEOOOOO", password_confirmation: "NEEEEOOOOO")
-      user.save
+      first_user.save
 
       second_user = User.new(first_name: "RealKeanu", last_name: "Reeves", email: "KEANUizCOOL@icloud.me", password: "NEEEEOOOOO", password_confirmation: "NEEEEOOOOO")
-      expect(user.errors.full_messages).not_to be_empty
+      second_user.save
+      expect(second_user.errors.full_messages).not_to be_empty
     end
   end
 end

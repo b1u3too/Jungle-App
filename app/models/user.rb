fa9b62c084e5dc7c564f910886188e7951f0f8d9
b1u3too <almadsen792@live.ca>
2022-07-21 04:35:55 +0000

    implement authenticate_with_credentials class method

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,13 @@ class User < ApplicationRecord
     presence: true,
     length: { minimum: 10 }
   validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    user = self.find_by_email(email)
+    if user && user.authenticate(password)
+      return user
+    end
+
+    nil
+  end
 end

461f97f9a15c44e6ab989eced753d9d3d2ec33c0
b1u3too <almadsen792@live.ca>
2022-07-22 03:52:13 +0000

    cleanup: removed superfluous line

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    @current_user = nil
     redirect_to '/login', notice: 'You have logged out'
   end
 end

c934d18a5c42750874f09b288b9be4760e8a38c8
b1u3too <almadsen792@live.ca>
2022-07-22 03:29:35 +0000

    fix: adjust class method syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,8 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true
 
   def self.authenticate_with_credentials(email, password)
-    user = self.find_by_email(email)
+    user = User.find_by_email(email)
+
     if user && user.authenticate(password)
       return user
     end
